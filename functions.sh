f52795d35e3b6efc6e1d67bbd1a8717bf067a7ec
Keshi Dai <keshi@spotify.com>
2019-08-10 16:02:52 -0400

    Add cd function to activate virtualenv

--- functions.sh
+++ functions.sh
@@ -14,3 +14,22 @@ function fmt_name() {
     FILE="$1";
     mv "$FILE" `echo $FILE | tr '[:blank:]' '_' | tr '[:upper:]' '[:lower:]'`;
 }
+
+function cd() {
+  builtin cd "$@"
+
+  if [[ -z "$VIRTUAL_ENV" ]] ; then
+      ## If env folder is found then activate the vitualenv
+      if [[ -d ./venv ]] ; then
+        source ./venv/bin/activate
+      fi
+  else
+      ## check the current folder belong to earlier VIRTUAL_ENV folder
+      # if yes then do nothing
+      # else deactivate
+      parentdir="$(dirname "$VIRTUAL_ENV")"
+      if [[ "$PWD"/ != "$parentdir"/* ]] ; then
+        deactivate
+      fi
+  fi
+}

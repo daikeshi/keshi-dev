c8b179efeb4306481df41ad3d425ab7047ed2282
Keshi Dai <keshi@spotify.com>
2021-04-12 13:34:24 -0400

    Disable PYENV_VIRTUALENV_DISABLE_PROMPT

--- common_settings.sh
+++ common_settings.sh
@@ -42,6 +42,7 @@ HISTFILESIZE=5000
 ### Config for pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
+export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 eval "$(pyenv init -)"
 # eval "$(pyenv virtualenv-init -)"
 pyenv activate sys
